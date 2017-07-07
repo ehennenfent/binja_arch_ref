1879ac0d2cfab9175ffc51b74fcd1e42a7c3dbe1
Eric Hennenfent <Eric.Hennenfent@nccgroup.trust>
2017-07-07 15:32:13 -0500

    Added several things. See long message.
    Added sizes to registers
    Added default integer size
    Properly retrieves name of endianness
    Better formatting for link register
    Added default calling convention
    Sort calling conventions so default always appears on top

--- __init__.py
+++ __init__.py
@@ -9,14 +9,15 @@ def render(bv):
     arch = bv.arch
 
     markdown = build_markdown(markdown, "# {} Cheat Sheet".format(arch.name))
-    markdown = build_markdown(markdown, "#### Address Size: {}".format(arch.address_size))
-    markdown = build_markdown(markdown, "#### Endianness: {}".format(arch.endianness))
-
-    markdown = build_markdown(markdown, "#### Link Register: {}".format(arch.link_reg))
+    markdown = build_markdown(markdown, "#### Address Size: {} bytes".format(arch.address_size))
+    markdown = build_markdown(markdown, "#### Default Integer Size: {} bytes".format(arch.default_int_size))
+    markdown = build_markdown(markdown, "#### Endianness: {}".format(arch.endianness.name))
+    markdown = build_markdown(markdown, "#### Link Register: {}".format("None" if 'temp' in arch.link_reg else arch.link_reg))
     markdown = build_markdown(markdown, "#### Stack Pointer: {}".format(arch.stack_pointer))
+    markdown = build_markdown(markdown, "#### Platform Default Calling Convention: {}".format(bv.platform.default_calling_convention.name))
 
     markdown = build_markdown(markdown, "## Calling Conventions")
-    for convention in bv.arch.calling_conventions:
+    for convention in sorted(bv.arch.calling_conventions.keys(), cmp=(lambda k, _: 1 if (k == bv.platform.default_calling_convention.name) else -1), reverse=True):
         convention = bv.arch.calling_conventions[convention]
         markdown = build_markdown(markdown, "### {}".format(convention.name))
 
@@ -44,7 +45,7 @@ def render(bv):
 
     markdown = build_markdown(markdown, "#### Full width registers:")
     for reg in bv.arch.full_width_regs:
-        markdown = build_markdown(markdown, '    ' + reg)
+        markdown = build_markdown(markdown, "    {register}  --  {size} bits".format(register=reg, size=(bv.arch.regs[reg].size * 8)))
 
     show_markdown_report(bv.arch.name + " Architecture Reference", markdown)
 
